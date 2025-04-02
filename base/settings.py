a5a8ea5880c7878a307601c07dc6624922fd2501
Beki <uzbekitube@gmail.com>
2025-04-02 09:30:26 +0900

    DB changed to postgres

--- base/settings.py
+++ base/settings.py
@@ -32,8 +32,12 @@ DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")  # when sending email to us
 # Database: https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST", "localhost"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
     }
 }
 
--- base/views.py
+++ base/views.py
@@ -4,11 +4,17 @@ from exchange.models import Request
 
 class IndexView(TemplateView):
     template_name = "index.html"
-    extra_context = {
-        "send_requests": Request.objects.filter(type="send", status="active").order_by(
-            "-created_at"
-        ),
-        "receive_requests": Request.objects.filter(
-            type="receive", status="active"
-        ).order_by("-created_at"),
-    }
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx.update(
+            {
+                "send_requests": Request.objects.filter(
+                    type="send", status="active"
+                ).order_by("-created_at"),
+                "receive_requests": Request.objects.filter(
+                    type="receive", status="active"
+                ).order_by("-created_at"),
+            }
+        )
+        return ctx
