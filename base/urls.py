24bf06d1bb8ef150c279aad6347dc20dc2e9f4d0
Beki <uzbekitube@gmail.com>
2025-04-02 19:44:38 +0900

    styles fixed, header cleaned up, light, dark mode added

--- base/urls.py
+++ base/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic.base import TemplateView
@@ -16,3 +18,7 @@ urlpatterns = [
     path("i18n/", include("django.conf.urls.i18n")),
     path("exchange/", include("exchange.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
