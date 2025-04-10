d2e17df64caccf5a37b594b3f83e118cb7be6398
Beki <uzbekitube@gmail.com>
2025-04-10 11:33:10 +0900

    contacts shown, admin panel fixed, bootstrap icons load replaced with only necessary icon svgs

--- base/users/admin.py
+++ base/users/admin.py
@@ -1,7 +1,36 @@
 from base.users.models import User
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    pass
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (
+            _("Personal info"),
+            {"fields": ("first_name", "last_name", "email", "phone", "phone_verified")},
+        ),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "usable_password", "password1", "password2"),
+            },
+        ),
+    )
--- base/users/models.py
+++ base/users/models.py
@@ -8,3 +8,11 @@ class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     phone = models.CharField(max_length=16, unique=True, blank=True, null=True)
     phone_verified = models.BooleanField(default=False)
+
+    def contact_info(self):
+        return dict(
+            phone=self.phone if self.phone_verified else None,
+            email=self.email if self.email else None,
+            username=self.username if self.username else None,
+            social_accounts=self.social_accounts.all() if hasattr(self, "social_accounts") else [],
+        )
